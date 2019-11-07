886f22592fb089204ba88f0c83075e5cf3469412
Leqier <triksiand@gmail.com>
2019-11-07 22:21:52 +0100

    leaves particles

---
+++ rouglikeFarmer/Assets/LeavesParticles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeavesParticles : MonoBehaviour
+{
+    void Awake()
+    {
+
+        gameObject.GetComponent<ParticleSystem>().Play();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

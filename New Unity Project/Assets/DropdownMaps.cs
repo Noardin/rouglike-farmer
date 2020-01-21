6f11103fcd378be2c581cccabb40af0d541f7a2b
Leqier <triksiand@gmail.com>
2020-01-21 21:52:41 +0100

    DandD

---
+++ New Unity Project/Assets/DropdownMaps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownMaps : MonoBehaviour
+{
+   public Dropdown dropdown;
+   public ImageLoader ImageLoader;
+
+   private void Start()
+   {
+      PopulateList();
+   }
+
+   public void PopulateList()
+   {
+      List<string> names = ImageLoader.GetMapsList();
+      dropdown.options = new List<Dropdown.OptionData>();
+      dropdown.AddOptions(names);
+   }
+
+   public void ListChoice(int item)
+   {
+      Debug.Log("item " + item);
+   }
+}
---
+++ New Unity Project/Assets/ImageLoader.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using System.IO;
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.Networking;
+
+
+public class ImageLoader : MonoBehaviour
+{
+   private string path;
+   public string Filename = "newfile.png";
+   public RawImage image;
+   public DropdownMaps dropdownMaps;
+
+
+
+   private void Start()
+   {
+      Deletes();
+   }
+
+   public void OpenExplorer()
+   {
+      path = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
+      SaveImage(path, Filename);
+   }
+
+   public void EditImageName(string filename)
+   {
+      Debug.Log("f"+filename);
+      Filename = filename;
+   }
+
+   void SaveImage(string path, string filename)
+   {
+      string FolderPath = Application.persistentDataPath + "/";
+      string newImagePath = FolderPath + filename + ".png";
+      if (path != null & System.IO.File.Exists(path))
+      {
+         int filecount = 0;
+         while (System.IO.File.Exists(newImagePath))
+         {
+            filecount++;
+            newImagePath = FolderPath + filename + filecount+".png";
+         }
+
+         if (filecount > 0)
+         {
+            filename += filecount.ToString();
+         }
+         
+         System.IO.File.Copy(path, newImagePath);
+         string mapsPath = Application.persistentDataPath + "/MapsImageData.txt";
+         MapsImageData mapsImageData = new MapsImageData();
+         mapsImageData.filename = filename;
+         mapsImageData.filepath = newImagePath;
+         string json = JsonUtility.ToJson(mapsImageData);
+         using (StreamWriter w = File.AppendText(mapsPath))
+         {
+            w.WriteLine(json);
+         }
+
+         dropdownMaps.PopulateList();
+         StartCoroutine(GetImage(filename));
+         
+      }
+   }
+
+   public IEnumerator GetImage(string filename)
+   {
+      string FolderPath = Application.persistentDataPath + "/"+filename+".png";
+      using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(FolderPath))
+      {
+         yield return uwr.SendWebRequest();
+
+         if (uwr.isNetworkError || uwr.isHttpError)
+         {
+            Debug.Log(uwr.error);
+         }
+         else
+         {
+            // Get downloaded asset bundle
+            var texture = DownloadHandlerTexture.GetContent(uwr);
+            displayImage(texture);
+         }
+      }
+      
+   }
+
+   public void displayImage(Texture2D t)
+   {
+      image.texture = t;
+   }
+
+   void Deletes()
+   {
+      string mapsPath = Application.persistentDataPath;
+      DirectoryInfo di = new DirectoryInfo(mapsPath);
+      foreach (var file in di.GetFiles())
+      {
+         file.Delete();
+      }
+     
+   }
+
+   public List<string> GetMapsList()
+   {
+      List<string> mapslist = new List<string>();
+      string mapsPath = Application.persistentDataPath + "/MapsImageData.txt";
+      using (StreamReader r = new StreamReader(mapsPath))
+      {
+         string line;
+         while ((line = r.ReadLine()) != null)
+         {
+            MapsImageData data = JsonUtility.FromJson<MapsImageData>(line);
+            mapslist.Add(data.filename);
+         }
+      }
+
+      return mapslist;
+
+   }
+}
+
+[Serializable]
+public class MapsImageData
+{
+   public string filename;
+   public string filepath;
+}
+
